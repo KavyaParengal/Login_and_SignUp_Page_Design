49cef71156a7ebc221c55cebd9d793c02a3b1edd
kavya <kavyaparengal9037@gmail.com>
2023-06-23 20:01:42 +0530

    second commit

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -8,6 +8,77 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+
+  String output = "0";
+  String history='';
+  String _output = "";
+  int num1 = 0;
+  int num2 = 0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "C") {
+      _output = "0";
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    }
+    else if (buttonText == "AC") {
+      _output = "0";
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+      history='';
+    }
+    else if (buttonText == "+" || buttonText == "-" || buttonText == "/" || buttonText == "*" || buttonText=='%') {
+      num1 = int.parse(output);
+      operand = buttonText;
+      _output = "";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        print("Already conatains a decimals");
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = int.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+        history=num1.toString() + operand.toString() + num2.toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+        history=num1.toString() + operand.toString() + num2.toString();
+      }
+      if (operand == "*") {
+        _output = (num1 * num2).toString();
+        history=num1.toString() + operand.toString() + num2.toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+        history=num1.toString() + operand.toString() + num2.toString();
+      }
+      if (operand == "%") {
+        _output = (num1 % num2).toString();
+        history=num1.toString() + operand.toString() + num2.toString();
+      }
+      // num1 = 0;
+      // num2 = 0;
+      // operand = "";
+    } else {
+      _output = int.parse(output + buttonText).toString();
+    }
+
+    print(_output);
+
+    setState(() {
+      output = _output;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,12 +86,24 @@ class _CalculatorState extends State<Calculator> {
       body: Column(
         children: [
           Container(
-            height: (MediaQuery.of(context).size.height * 0.3),
-            //color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 150,right: 20),
+              child: Text(history,style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w400),
+              ),
+            ),
+            alignment: Alignment(1.0,1.0),
           ),
-          SizedBox(
-            height: 10,
+          Container(
+            //height: (MediaQuery.of(context).size.height * 0.3),
+            child: Padding(
+              padding: const EdgeInsets.only(right: 20,top: 20,left: 30),
+              child: Text(output,style: TextStyle(color: Colors.white,fontSize: 33,fontWeight: FontWeight.bold),
+              ),
+            ),
+            alignment: Alignment(1.0,1.0),//color: Colors.white,
           ),
+          //Expanded(child: Divider(color: Colors.grey,thickness: 1,)),
+          SizedBox(height: 40,),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
@@ -42,7 +125,6 @@ class _CalculatorState extends State<Calculator> {
                 buildCircleAvatar('8', Colors.deepPurple),
                 buildCircleAvatar('9', Colors.deepPurple),
                 buildCircleAvatar('*', Colors.purpleAccent)
-
               ],
             ),
           ),
@@ -55,7 +137,6 @@ class _CalculatorState extends State<Calculator> {
                 buildCircleAvatar('5', Colors.deepPurple),
                 buildCircleAvatar('6', Colors.deepPurple),
                 buildCircleAvatar('-', Colors.purpleAccent)
-
               ],
             ),
           ),
@@ -68,7 +149,6 @@ class _CalculatorState extends State<Calculator> {
                 buildCircleAvatar('2', Colors.deepPurple),
                 buildCircleAvatar('3', Colors.deepPurple),
                 buildCircleAvatar('+', Colors.purpleAccent)
-
               ],
             ),
           ),
@@ -92,9 +172,8 @@ class _CalculatorState extends State<Calculator> {
   CircleAvatar buildCircleAvatar(String calculatortxt,Color clrbutton) {
     return CircleAvatar(
                 backgroundColor: clrbutton,
-                radius: 45,
-                child: Text(
-                  calculatortxt,
+                radius: 42,
+                child: TextButton(child: Text(calculatortxt,
                   style: TextStyle(
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
@@ -102,6 +181,12 @@ class _CalculatorState extends State<Calculator> {
                   ),
                   textAlign: TextAlign.center,
                 ),
+                onPressed: (){
+                  buttonPressed(calculatortxt);
+                }
+                  ,),
               );
   }
 }
+
+
